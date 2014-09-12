dc6712d7491cf7191af7e6e1c2805c22ac6699dd
marat <test@test.test>
2014-09-12 06:39:10 +0000

    -

--- marat/do.sh
+++ marat/do.sh
@@ -14,6 +14,7 @@ EOF
 if [ "x$1" = "xhelp" ] || [ "x$1" = "x" ]; then
     help
 elif [ "x$1" = "xpush" ]; then
+	/opt/bitnami/mysql/bin/mysqldump -u root -pbitnami --skip-extended-insert bitnami_joomla > /opt/bitnami/apps/joomla/htdocs/database.sql
 	git add --all
 	git commit -a -m '-'
 	git push origin master	
